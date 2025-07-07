2db382b4695c1fb5d22ca22f0d6b67d41c757cf5
Jose Joya <ing.jose.joya@gmail.com>
2025-07-07 01:33:48 -0500

    Create astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ZunoFramework.github.io",
+  base: "zuno-docs",
   integrations: [
     starlight({
       title: "Zuno Framework",
