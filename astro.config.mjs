6e7cc4adf57f8df89307a0bb74e0aae38733c775
Jose Joya <jose.joya@globant.com>
2025-07-08 16:58:25 -0500

    Feat: Added new documentation structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,23 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ZunoFramework.github.io",
-  base: "zuno-docs",
   integrations: [
     starlight({
+      locales: {
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        es: {
+          label: "Español",
+          lang: "es",
+        },
+      },
+      defaultLocale: "en",
       title: "Zuno Framework",
       description:
         "Zuno is a minimal C++ web framework focused on speed, simplicity, and full developer control. Learn how to build fast, modular servers with clean and expressive syntax.",
